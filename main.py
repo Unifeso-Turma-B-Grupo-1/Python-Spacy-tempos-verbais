2d8156dce4e17a038cb4c57a9aa40da8133967dc
ogooota <andreaneto263@gmail.com>
2025-08-23 22:41:19 -0300

    Implementação de funções
    Substituída a file dic.py por dicionarios.json;
    Adicionado key "ignore" em dicionarios.json para caracteres ignorados;
    Removido .txt "texto_a_ser_lido";
    Trocado o nome dos arrays globais para padrão de produção "g_";
    Escolha de file .txt com tkinter ou digitação no console;
    Implementada contagem de palavras no texto;
    Substituído if - else por match - case;
    Consertado "palavras" por "verbos" na função classify_verbs();

--- main.py
+++ main.py
@@ -1,70 +1,124 @@
-import sys
-sys.stdout.reconfigure(encoding='utf-8') #encoda caracteres especiais
+import sys, json, spacy, time;
+import tkinter as tk;
+from tkinter import filedialog;
+sys.stdout.reconfigure(encoding='utf-8'); #encoda caracteres especiais
 
+# arrays para tempos verbais
+g_Simple_present = []
+g_Present_continuous = []
+g_Simple_past = []
+g_Past_participle = []
 
-import spacy
-from dic import pos_pt, dep_pt, tag_pt, verbo_tempo #importa lib que eu fiz com as palavras traduzidas pra pt br
-
-simple_present = []
-present_continuous = []
-simple_past = []
-past_participle = []
-
-with open("texto_a_ser_lido.txt", "r", encoding="utf-8") as f: 
-    conteudo = f.read()
-
-
-nlp = spacy.load("en_core_web_sm")
-doc = nlp(conteudo)
-
-for token in doc:
-    tag = token.tag_
-    tk = token.text
+#carregando json
+try:
+    with open("dicionarios.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+except json.JSONDecodeError as e:
+    print("Erro ao carregar 'dicionarios.json': " + e)
     
-    print(
-    f"Palavra: {token.text}\n"
-    f"Infinitivo: {token.lemma_}\n"
-    f"Classe gramatical: {spacy.explain(token.pos_)}" +
-        (f" -> {pos_pt.get(token.pos_)}" if token.pos_ in pos_pt else "") + "\n"
-    f"Detalhes (tag): {spacy.explain(token.tag_)}" +
-        (f" -> {verbo_tempo.get(token.tag_, spacy.explain(token.tag_))}" if token.tag_ in verbo_tempo else "") + "\n"
-    f"Dependência sintática: {spacy.explain(token.dep_)}" +
-        (f" -> {dep_pt.get(token.dep_, spacy.explain(token.dep_))}" if token.dep_ in dep_pt else "") + "\n"
-    "----------------------------------------"
-)
+# função __init__ indica construtor: função que é chamada sempre que o programa for iniciado.
+def __init__():
+    nlp = spacy.load("en_core_web_sm")
+    type_pick = str(input("Você quer digitar uma frase ou escolher um arquivo .txt neste computador? ([e]scolher/[d]igitar)\n"))
+    if type_pick in "dD":
+        conteudo = str(input("Digite a frase: \n"))
+    elif type_pick in "eE":
+        root = tk.Tk()
+        root.withdraw()
+        find_file = filedialog.askopenfilename(
+            title="Selecione um arquivo .txt",
+            filetypes=[("Arquivos de texto", "*.txt")]
+        )
+        if find_file:
+            with open(find_file, "r", encoding="utf-8") as f:
+                conteudo = f.read()
+            print(find_file)
+            for c in find_file.split('/'):
+                if ".txt" in c:
+                    print(f"-> Arquivo encontrado: {c}")
+        else:
+            print("Não foi possível acessar o arquivo.")
+            sys.exit()
+    doc = nlp(conteudo)
+    if show_content(doc):
+        classify_verbs()
+        input("Pressione qualquer tecla para terminar o programa...")
+    else:
+        print("Erro ao mostrar conteúdo.")
 
-    if tag in ["VBZ", "VBP"]:
-        simple_present.append(tk)
-    elif tag == "VBG":
-        present_continuous.append(tk)
-    elif tag == "VBD":
-        simple_past.append(tk)
-    elif tag == "VBN":
-        past_participle.append(tk)
+# func pra mostrar o conteudo do spacy doc
+def show_content(doc):
+    ignored_values = data["ignore"].values()
+    ignorados = 0
+    for c in doc:
+        if c.text.lower() in ignored_values:
+            ignorados+=1
+    time.sleep(1.5)
+    print(f"-> Foram encontradas {len(doc)-ignorados} palavras no seu texto!")
+    for token in doc:
+        tag = token.tag_
+        tk = token.text
+        # ignorar caracteres inuteis para a análise
+        if tk.lower() in ignored_values:
+            print(f"found ignored value -> {tk.lower()}")
+            continue
+        print('-'*60)
+        print(f"Palavra: {token.text}")
+        # o "infinitivo" só deve ser mostrado caso seja um verbo,
+        # caso seja outra classe de palavra, mostrar "Palavra sem declinação:"
+        if token.pos_ == "VERB":
+            print(f"Infinitivo: to {token.lemma_}")
+        else:
+            print(f"Palavra sem declinação/contração: {token.lemma_}")
+        print(
+            f"Classe gramatical: {spacy.explain(token.pos_)}" +
+            (f" -> {data["pos_pt"].get(token.pos_)}" if token.pos_ in data["pos_pt"] else "") + "\n"
+            f"Detalhes (tag): {spacy.explain(token.tag_)}" +
+            (f" -> {data["verbo_tempo"].get(token.tag_, spacy.explain(token.tag_))}" if token.tag_ in data["verbo_tempo"] else "") + "\n"
+            f"Dependência sintática: {spacy.explain(token.dep_)}" +
+            (f" -> {data["dep_pt"].get(token.dep_, spacy.explain(token.dep_))}" if token.dep_ in data["dep_pt"] else "") + "\n" +
+            "-"*60
+        )
+        match tag:
+            case ["VBZ", "VBP"]:
+                g_Simple_present.append(tk)
+            case "VBG":
+                g_Present_continuous.append(tk)
+            case "VBD":
+                g_Simple_past.append(tk)
+            case "VBN":
+                g_Past_participle.append(tk)
+        time.sleep(1)
+    return True
 
-frase = "Não houveram palavras no"
+def classify_verbs():
+    frase = "Não houveram verbos no"
+     
+    print("----- [VERBOS NO SIMPLE PRESENT] ----- (ex: runs)")
+    if g_Simple_present:
+        for word in g_Simple_present:
+            print("-"*5 + word + "-"*5)
+    else:
+        print(f"{frase} simple present")
 
-print("Quais palavras estão no Simple Present? (ex: runs)")
-if simple_present:
-    print(simple_present)
-else:
-    print(f"{frase} simple present")
+    print("\n----- [VERBOS NO PRESENT CONTINUOUS] ----- (ex: running)")
+    if g_Present_continuous:
+        for word in g_Present_continuous:
+            print("-"*5 + word + "-"*5)
+    else:
+        print(f"{frase} simple continuous")
 
-print("\nQuais palavras estão no Present Continuous? (ex: running)")
-if present_continuous:
-    print(present_continuous)
-else:
-    print(f"{frase} simple continuous")
+    print("\n----- [VERBOS NO SIMPLE PAST] ----- (ex: ran)")
+    if g_Simple_past:
+        for word in g_Simple_past:
+            print("-"*5 + word + "-"*5)
+    else:
+        print(f"{frase} simple past")
 
-print("\nQuais palavras estão no Simple Past? (ex: ran)")
-if simple_past:
-    print(simple_past)
-else:
-    print(f"{frase} simple past")
-
-print("\nQuais palavras estão no Past Participle? (ex: eaten)")
-if past_participle:
-    print(past_participle)
-else:
-    print(f"{frase} simple participle")
-
+    print("\n----- [VERBOS NO PAST PARTICIPLE] ----- (ex: eaten)")
+    if g_Past_participle:
+        for word in g_Past_participle:
+            print("-"*5 + word + "-"*5)
+    else:
+        print(f"{frase} simple participle")
+__init__()
